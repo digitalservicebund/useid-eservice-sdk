b949916866886e1fd928645b88f661352e823497
Simon Knott <info@simonknott.de>
2023-03-06 14:48:30 +0100

    fix: use local axios instance

--- src/UseIdAPI.test.ts
+++ src/UseIdAPI.test.ts
@@ -17,13 +17,14 @@ describe('startSession()', () => {
   const tcTokenUrl = "mocked tc token url";
   let axiosPost: jest.SpyInstance;
   beforeEach(() => {
-    axiosPost = jest.spyOn(axios, 'post').mockImplementation(jest.fn(() => Promise.resolve({ data: { tcTokenUrl } })) as jest.Mock);
+    axiosPost = jest.spyOn(useIdAPI.api, 'post').mockImplementation(jest.fn(() => Promise.resolve({ data: { tcTokenUrl } })) as jest.Mock);
   });
 
   it('should start session and return tcTokenUrl', async () => {
     const response = await useIdAPI.startSession();
     expect(response).toEqual({ tcTokenUrl });
-    expect(axiosPost).toHaveBeenCalledWith(`${useIdAPI.domain}/api/v1/identification/sessions`);
+    expect(useIdAPI.api.getUri()).toEqual(`${useIdAPI.domain}/api/v1/identification/sessions`);
+    expect(axiosPost).toHaveBeenCalledWith('/');
   });
 });
 
@@ -31,14 +32,14 @@ describe('getIdentity()', () => {
   const identityValues = { dg1: "dg1", dg2: "dg2" };
   let axiosGet: jest.SpyInstance;
   beforeEach(() => {
-    axiosGet = jest.spyOn(axios, 'get').mockImplementation(jest.fn(() => Promise.resolve({ data: identityValues })) as jest.Mock);
+    axiosGet = jest.spyOn(useIdAPI.api, 'get').mockImplementation(jest.fn(() => Promise.resolve({ data: identityValues })) as jest.Mock);
   });
 
   it('should start session and return tcTokenUrl', async () => {
     const eIdSessionId = "mocked eIdSessionId";
     const response = await useIdAPI.getIdentity(eIdSessionId);
-    expect(response).toEqual(new Identity(identityValues));
-    expect(axiosGet).toHaveBeenCalledWith(`${useIdAPI.domain}/api/v1/identification/sessions/${eIdSessionId}`);
+    expect(response).toEqual(new Identity(identityValues as any));
+    expect(axiosGet).toHaveBeenCalledWith(`/${eIdSessionId}`);
   });
 });
 
